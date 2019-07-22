83fa738431106ef115389fbe0587e16091aae2cf
Joe Szafarowicz <joeszaf+github@gmail.com>
2019-07-22 16:27:37 -0400

    handle patron and selling tickets to patrons in Theater class

--- Theater.py
+++ Theater.py
@@ -12,7 +12,18 @@ class Theater:
     def get_shows(self):
         return self.__shows
 
-    # def sell_ticket(self, patron):
-    #     for show in self.__shows:
-    #         if
+    def get_earnings(self):
+        return self.__earnings
+
+    def sell_ticket(self, patron):
+        for show in self.__shows:
+            if patron.match_preference(show) and patron.match_time_availability(show):
+                if show.add_patron(patron):
+                    self.__earnings += show.get_ticket_price()
+                    break
+        self.__patrons_not_seeing_shows.append(patron)
+
+    def handle_patrons(self, patrons):
+        for patron in patrons:
+            self.sell_ticket(patron)
 
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from datetime import datetime
-
-date_format = "%m-%d-%Y %H:%M:%S"
+from Patron import Patron
+from Theater import Theater
+from Show import Show
+from TimeRange import TimeRange
 
 def main():
-    start = datetime.strptime("07-01-2019 08:30:00", date_format)
-    end = datetime.strptime("07-01-2019 10:0:00", date_format)
-    diff = end - start
+    patron1 = Patron("Janes", "Lashe", "jlashe@gmail.com", preferences=["improv"])
+    patron2 = Patron("Callahan", "Burgess", "cburg@gmail.com", preferences=["standup", "drama"])
+    patron3 = Patron("Gaugino", "Julio", "cjules@gmail.com")
+    patron4 = Patron("Wilhelmina", "Smith", "wilhelmsmith@gmail.com",
+                          [("07-01-2019 10:00", "07-01-2019 17:00"), ("07-01-2019 18:30", "07-01-2019 22:00")],
+                          ["improv", "sketch", "standup"])
 
-    print( end > start)
+    show1 = Show("Mainstage", "07-01-2019 20:00", "07-01-2019 21:30", 20, 90, "improv")
+    show2 = Show("Comedy Lab", "07-01-2019 19:30", "07-01-2019 21:00", 15, 40)
+    show3 = Show("Peoples Show", "07-01-2019 21:30", "07-01-2019 23:00", 5, 40, "standup")
+    show4 = Show("Sketch Planet", "07-01-2019 18:00", "07-01-2019 19:30", 15, 40, "sketch")
+
+    shows = [show1, show2, show3, show4]
+    patrons =[patron1, patron2, patron3, patron4]
+
+    theater = Theater("ImprovBoston", shows)
+    theater.handle_patrons(patrons)
+    print(theater.get_earnings())
+
 
 
 
